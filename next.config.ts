f55676e658bd8cd89283f7fa84d8b0a1c016b7cf
duc1512 <duc151220036987@gmail.com>
2026-01-15 10:59:30 +0700

    Update next/image remotePattern

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,11 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "calm-cendol-f3d19f.netlify.app", 
+        pathname: "/**",
+      },
     ],
   },
 };
