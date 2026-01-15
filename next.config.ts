fc891aa39c35d445176d0d59396ae64d06fd3c05
duc1512 <duc151220036987@gmail.com>
2026-01-15 10:55:59 +0700

    Update next/image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['image.tmdb.org', 'calm-cendol-f3d19f.netlify.app'],
-    deviceSizes: [320, 420, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
